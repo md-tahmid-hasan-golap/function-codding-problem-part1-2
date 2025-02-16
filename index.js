b8301b62bc8385305c07bb27b47efcf313e04e54
golap-raj <golapraj47@gmail.com>
2025-02-17 00:51:59 +0600

    add index

---
+++ index.js
@@ -0,0 +1,43 @@
+function square (number){
+
+    const borgo = number * number;
+    console.log(borgo)
+
+}
+
+// square(19)
+// square(10)
+// square(25)
+// square(33)
+// square(40)
+
+function add (num1, num2){
+    const sum = num1 + num2;
+    console.log(sum)
+}
+
+// add(12, 44);
+// add(667, 333);
+// add(727, 44);
+// add(10, 10)
+
+function add (num1, num2){
+    const diff = num1 - num2;
+    console.log(diff)
+
+}
+
+// add(3344, 66);
+// add(123, 55);
+// add(30, 10);
+// add(200, 300)
+
+function addAll(a, b, c, d, e){
+    const total = a + b + c + d + e;
+    console.log(total)
+
+}
+
+addAll(12, 44, 55, 66, 43);
+addAll(11, 44, 566,7, 43);
+addAll(10, 30, 10, 30, 10)
